7e66c8739c6699cf623d9c19352245db75cb1289
Jeet Sukumaran <jeetsukumaran@gmail.com>
2015-11-11 02:29:12 -0500

    [WIP] RevBayes biogeography output parser

--- inphest/revbayes.py
+++ inphest/revbayes.py
@@ -25,14 +25,31 @@ import dendropy
 class RevBayesBiogeographyParser(object):
 
     EVENT_PATTERN = re.compile(r"{(.*?)}")
+    TREE_SAMPLES_FIELDNAMES = ("tree_idx", "iteration", "posterior", "ln_likelihood", "prior")
+    EDGE_SAMPLES_FIELDNAMES = (
+            "tree_idx",
+            "edge_id",
+            "split_bitstring",
+            "leafset_bitstring",
+            "edge_starting_age",
+            "edge_ending_age",
+            "edge_duration",
+            "child0_edge_id",
+            "child1_edge_id",
+            # "edge_revbayes_index",
+            "edge_starting_state",
+            "edge_ending_state",
+            "edge_cladogenetic_speciation_mode",
+            )
+    NULL_VALUE = "NA"
 
     def __init__(self):
         ## information on each tree in sample
-        self.tree_samples = []
+        self.tree_entries = []
         ## events organized by tree
-        self.event_schedules_by_tree = {}
+        self.event_schedules_by_tree = []
         ## information on each bipartition across all samples
-        self.bipartition_samples = []
+        self.edge_entries = []
         ## events organized by bipartition
         self.event_schedules_by_edge = collections.defaultdict(dendropy.Bipartition)
 
@@ -52,14 +69,14 @@ class RevBayesBiogeographyParser(object):
             parts = row.split("\t")
             iteration, posterior, likelihood, prior, tree_str = row.split("\t")
 
-            tree_sample_entry = {}
-            tree_sample_entry["tree_idx"] = tree_idx
-            tree_sample_entry["iteration"] = float(iteration)
-            tree_sample_entry["posterior"] = float(posterior)
-            tree_sample_entry["ln_likelihood"] = float(likelihood)
-            tree_sample_entry["prior"] = float(prior)
+            tree_entry = {}
+            tree_entry["tree_idx"] = tree_idx
+            tree_entry["iteration"] = float(iteration)
+            tree_entry["posterior"] = float(posterior)
+            tree_entry["ln_likelihood"] = float(likelihood)
+            tree_entry["prior"] = float(prior)
 
-            self.tree_samples.append(tree_sample_entry)
+            self.tree_entries.append(tree_entry)
             tree = dendropy.Tree.get(
                     data=tree_str,
                     schema="newick",
@@ -72,40 +89,67 @@ class RevBayesBiogeographyParser(object):
             # print(tree.as_string("newick", suppress_annotations=True))
             tree.calc_node_ages(ultrametricity_precision=0.01)
             for nd in tree:
-                edge_master_entry = {}
-                edge_master_entry["tree_idx"] = tree_idx
-                edge_master_entry["bipartition_idx"] = int(nd.edge.bipartition)
-                edge_master_entry["split"] = nd.edge.bipartition.split_as_bitstring
-                edge_master_entry["leafset"] = nd.edge.bipartition.leafset_as_bitstring
-                edge_master_entry["duration"] = nd.edge.length
+                edge_entry = {}
+                edge_entry["tree_idx"] = tree_idx
+                edge_entry["edge_id"] = int(nd.edge.bipartition)
+                edge_entry["split_bitstring"] = nd.edge.bipartition.split_as_bitstring()
+                edge_entry["leafset_bitstring"] = nd.edge.bipartition.leafset_as_bitstring()
+                edge_entry["edge_duration"] = nd.edge.length
+                edge_entry["edge_ending_age"] = nd.age
                 if nd.parent_node:
-                    edge_master_entry["starting_age"] = nd.parent_node.age
+                    edge_entry["edge_starting_age"] = nd.parent_node.age
                 else:
-                    edge_master_entry["starting_age"] = None
-                edge_master_entry["ending_age"] = nd.age
+                    # special case for root
+                    edge_entry["edge_starting_age"] = 0.0
+                    edge_entry["edge_duration"] = nd.age
                 if nd.is_leaf():
-                    edge_master_entry["child0"] = None
-                    edge_master_entry["child1"] = None
+                    edge_entry["child0_edge_id"] = RevBayesBiogeographyParser.NULL_VALUE
+                    edge_entry["child1_edge_id"] = RevBayesBiogeographyParser.NULL_VALUE
                 else:
                     for ch_idx, ch in enumerate(nd.child_node_iter()):
-                        edge_master_entry["child{}".format(ch_idx)] = int(ch.edge.bipartition)
+                        edge_entry["child{}_edge_id".format(ch_idx)] = int(ch.edge.bipartition)
                 edge_metadata, edge_events = self._extract_comment_metadata(nd)
-                edge_master_entry["revbayes_index"] = edge_metadata["index"]
-                edge_master_entry["starting_state"] = edge_metadata["pa"]
-                edge_master_entry["ending_state"] = edge_metadata["nd"]
+                # edge_entry["edge_revbayes_index"] = edge_metadata["index"]
+                edge_entry["edge_starting_state"] = edge_metadata["pa"]
+                edge_entry["edge_ending_state"] = edge_metadata["nd"]
                 if "cs" in edge_metadata:
                     if edge_metadata["cs"] == "s":
-                        edge_master_entry["cladogenetic_speciation_mode"] = "subset_sympatry"
+                        edge_entry["edge_cladogenetic_speciation_mode"] = "subset_sympatry"
                     elif edge_metadata["cs"] == "n":
-                        edge_master_entry["cladogenetic_speciation_mode"] = "narrow_sympatry"
+                        edge_entry["edge_cladogenetic_speciation_mode"] = "narrow_sympatry"
                     elif edge_metadata["cs"] == "w":
-                        edge_master_entry["cladogenetic_speciation_mode"] = "widespread_sympatry"
+                        edge_entry["edge_cladogenetic_speciation_mode"] = "widespread_sympatry"
                     elif edge_metadata["cs"] == "a":
-                        edge_master_entry["cladogenetic_speciation_mode"] = "allopatry"
+                        edge_entry["edge_cladogenetic_speciation_mode"] = "allopatry"
                     else:
                         raise ValueError("Unrecognized cladogenetic speciation mode event type: '{}'".format(edge_metadata["cs"]))
                 else:
-                    edge_master_entry["cladogenetic_speciation_mode"] = "NA"
+                    edge_entry["edge_cladogenetic_speciation_mode"] = RevBayesBiogeographyParser.NULL_VALUE
+                self.edge_entries.append(edge_entry)
+                for event in edge_events:
+                    event_entry = {}
+                    event_entry["tree_idx"] = edge_entry["tree_idx"]
+                    event_entry["edge_id"] = edge_entry["edge_id"]
+                    event_entry["age"] = event["age"]
+                    # event_entry["time"] = event["time"]
+                    if event["to_state"] == "1":
+                        event_entry["event_type"] = "anagenetic_area_gain"
+                    elif event["to_state"] == "0":
+                        event_entry["event_type"] = "anagenetic_area_loss"
+                    else:
+                        raise ValueError("Unexpected value for state: expecting '0' or '1' but found '{}'".format(event["to_state"]))
+                    event_entry["area_idx"] = event["area_idx"]
+                    event_entry["to_state"] = event["to_state"]
+                    self.event_schedules_by_tree.append(event_entry)
+                ## handle splitting event
+                if not nd.is_leaf():
+                    self.event_schedules_by_tree.append({
+                        "tree_idx": edge_entry["tree_idx"],
+                        "edge_id": edge_entry["edge_id"],
+                        "age": edge_entry["edge_ending_age"],
+                        "child0_edge_id": edge_entry["child0_edge_id"],
+                        "child1_edge_id": edge_entry["child1_edge_id"],
+                            })
 
     def _extract_comment_metadata(self, nd):
 
@@ -180,9 +224,41 @@ class RevBayesBiogeographyParser(object):
                     edge_events.append(event)
         return edge_metadata, edge_events
 
+    def store(self,
+            output_prefix,
+            delimiter="\t",
+            output_suffix="tsv",
+            ):
+        # tree samples
+        treef = open(output_prefix + ".tree-samples." + output_suffix, "w")
+        writer = csv.DictWriter(treef,
+                fieldnames=RevBayesBiogeographyParser.TREE_SAMPLES_FIELDNAMES,
+                restval=RevBayesBiogeographyParser.NULL_VALUE,
+                delimiter=delimiter,
+                lineterminator=os.linesep,
+                )
+        writer.writeheader()
+        writer.writerows(self.tree_entries)
+        treef.close()
+
+        # edge samples
+        # events_by_treef = open(output_prefix + ".tree-events." + output_suffix, "w")
+        edgef = open(output_prefix + ".edges." + output_suffix, "w")
+        writer = csv.DictWriter(edgef,
+                fieldnames=RevBayesBiogeographyParser.EDGE_SAMPLES_FIELDNAMES,
+                restval=RevBayesBiogeographyParser.NULL_VALUE,
+                delimiter=delimiter,
+                lineterminator=os.linesep,
+                )
+        writer.writeheader()
+        writer.writerows(self.edge_entries)
+        edgef.close()
+
+
 def main():
     rbp = RevBayesBiogeographyParser()
     rbp.parse(sys.argv[1])
+    rbp.store(sys.argv[2])
 
 if __name__ == "__main__":
     main()
